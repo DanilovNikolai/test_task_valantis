b9f189e713ad597648c03e54c9b8b8a32ac2927f
Danilov_Nikolai <dan1k91@mail.ru>
2024-02-28 20:13:44 +0300

    add_fetching_ids

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect } from "react";
+import "./App.css";
+import getLocalDate from "./utils/getLocalDate";
+import { md5 } from 'js-md5';
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  const currentDate = getLocalDate();
+  const xAuth = md5(import.meta.env.VITE_VALANTIS_API_KEY + "_" + currentDate);
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+  useEffect(() => {
+    fetch("https://api.valantis.store:41000/", {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+        "X-Auth": xAuth,
+      },
+      body: JSON.stringify({
+        action: "get_ids",
+        params: { offset: 10, limit: 10 },
+      }),
+    })
+      .then((response) => response.json())
+      .then((data) => data?.result.forEach((item: any) => console.log(item)
+      ));
+  }, []);
+
+  return <div></div>;
 }
 
-export default App
+export default App;
