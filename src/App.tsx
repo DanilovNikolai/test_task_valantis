14a684d443b82cd17f940a89a927e6545af65b8b
Danilov_Nikolai <dan1k91@mail.ru>
2024-02-29 16:24:12 +0300

    fetching_items_added

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,53 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
-import getLocalDate from "./utils/getLocalDate";
-import { md5 } from 'js-md5';
+import getLocalDate from "./utils/getLocalDate.ts";
+import { md5 } from "js-md5";
+import useFetch from "./hooks/useFetch.ts";
+import Loader from "./components/Loader/index.tsx";
 
+type Item = {
+  brand: string | null;
+  id: string;
+  price: number | null;
+  product: string;
+};
 
 function App() {
   const currentDate = getLocalDate();
   const xAuth = md5(import.meta.env.VITE_VALANTIS_API_KEY + "_" + currentDate);
+  const { post, loading } = useFetch();
+  const [items, setItems] = useState<Item[]>();
 
   useEffect(() => {
-    fetch("https://api.valantis.store:41000/", {
-      method: "POST",
-      headers: {
-        "content-type": "application/json",
-        "X-Auth": xAuth,
-      },
-      body: JSON.stringify({
-        action: "get_ids",
-        params: { offset: 10, limit: 10 },
-      }),
+    post(import.meta.env.VITE_VALANTIS_API_URL, xAuth, {
+      action: "get_ids",
+      params: { offset: 10, limit: 5 },
     })
-      .then((response) => response.json())
-      .then((data) => data?.result.forEach((item: any) => console.log(item)
-      ));
+      .then((data: unknown) => {
+        const itemsData = data as string[];
+        return post(import.meta.env.VITE_VALANTIS_API_URL, xAuth, {
+          action: "get_items",
+          params: { ids: itemsData },
+        });
+      })
+      .then((data: unknown) => {
+        const itemsData = data as Item[];
+        setItems(itemsData);
+      })
+      .catch((error) => console.error("Error fetching data:", error));
   }, []);
 
-  return <div></div>;
+  return (
+    <div>
+      {items && !loading ? (
+        items.map((item) => {
+          return <p key={item.id}>{item.product}</p>;
+        })
+      ) : (
+        <Loader />
+      )}
+    </div>
+  );
 }
 
 export default App;
