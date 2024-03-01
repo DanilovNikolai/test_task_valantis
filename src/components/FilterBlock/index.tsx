277f925963a56aaa8033b943c8f7596e9701fa5c
Danilov_Nikolai <dan1k91@mail.ru>
2024-03-01 21:34:10 +0300

    filter_logic_fixed

--- src/components/FilterBlock/index.tsx
+++ src/components/FilterBlock/index.tsx
@@ -1,6 +1,5 @@
 import styles from "./Filter.module.scss";
-import { useEffect, useState } from "react";
-import { ItemProps } from "../../App";
+import { useState } from "react";
 import { Action } from "../../hooks/useFetch";
 import { BodyParams } from "../../hooks/useFetch";
 
@@ -12,42 +11,53 @@ interface FilterBlockProps {
     arg1: string,
     arg2: Action,
     arg3: BodyParams
-  ) => string[] | ItemProps[];
+  ) => Promise<any>;
   loading: boolean;
+  setItems: (arg0: any) => void;
 }
 
-const FilterBlock: React.FC<FilterBlockProps> = (
-  {
-    // url,
-    // xAuth,
-    // post,
-    // loading,
-  }
-) => {
+const FilterBlock: React.FC<FilterBlockProps> = ({
+  url,
+  xAuth,
+  post,
+  loading,
+  setItems,
+}) => {
   const [inputValue, setInputValue] = useState<string>("");
-  const [selectValue, setSelectValue] = useState<string>("");
+  const [selectValue, setSelectValue] = useState<string>("product");
+  const [filterApplied, setFilterApplied] = useState<string>("");
 
-  useEffect(() => {
-    console.log(inputValue);
-    console.log(selectValue);
-  }, [inputValue, selectValue]);
-
-  function handleFilterButton(event: { preventDefault: () => void }) {
+  function handleFilterButton(
+    event: React.MouseEvent<HTMLButtonElement, MouseEvent>
+  ) {
     event.preventDefault();
-    // post(url, xAuth, "filter", { selectValue: inputValue }).then((data) =>
-    //   console.log(data)
-    // );
+    const params: BodyParams = {};
+
+    if (selectValue === "price") {
+      params[selectValue] = parseInt(inputValue, 10);
+    } else {
+      params[selectValue] = inputValue;
+    }
+
+    post(url, xAuth, "filter", params).then(async (data) => {
+      const items = await post(url, xAuth, "get_items", { ids: data });
+      setItems(items);
+      setFilterApplied(
+        `Применен фильтр ${selectValue} со значением ${inputValue}`
+      );
+    });
   }
 
   return (
     <div className={styles.root}>
       <div className={styles.container}>
-        <form action="">
+        <form action="" className={styles.filter_form}>
           <select
             name="filter"
             id="filter"
             value={selectValue}
             onChange={(e) => setSelectValue(e.target.value)}
+            className={styles.filter_select}
           >
             <option value="product">По названию</option>
             <option value="price">По цене</option>
@@ -57,11 +67,13 @@ const FilterBlock: React.FC<FilterBlockProps> = (
             type="text"
             value={inputValue}
             onChange={(e) => setInputValue(e.target.value)}
+            className={styles.filter_input}
           />
-          <button onClick={handleFilterButton} type="submit">
-            Поиск
+          <button onClick={handleFilterButton} type="submit" className={styles.filter_button}>
+            <span>Поиск</span>
           </button>
         </form>
+        {filterApplied && <div>{filterApplied}</div>}
       </div>
     </div>
   );
