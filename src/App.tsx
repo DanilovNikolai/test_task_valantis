606401ea8d1aaf1b30ce317a342425cca70d6bca
Danilov_Nikolai <dan1k91@mail.ru>
2024-03-03 22:05:53 +0300

    pagination_logic_fixed

--- src/App.tsx
+++ src/App.tsx
@@ -5,9 +5,10 @@ import { md5 } from "js-md5";
 import useFetch from "./hooks/useFetch.ts";
 
 import filterUniqueItems from "./utils/filterUniqueItems.ts";
-import Header from "./components/Header/index.tsx";
-import ItemsList from "./components/ItemsList/index.tsx";
-import FilterBlock from "./components/FilterBlock/index.tsx";
+import Header from "./components/Header";
+import ItemsList from "./components/ItemsList";
+import FilterBlock from "./components/FilterBlock";
+import Pagination from "./components/Pagination";
 
 export interface ItemProps {
   brand: string | null;
@@ -21,35 +22,83 @@ function App() {
   const xAuth = md5(import.meta.env.VITE_VALANTIS_API_KEY + "_" + currentDate);
   const url = import.meta.env.VITE_VALANTIS_API_URL;
   const { post, loading } = useFetch();
-  const [items, setItems] = useState<ItemProps[]>([]);
+  const [items, setItems] = useState<ItemProps[] | null>(() => {
+    const storedItems = localStorage.getItem("items");
+    return storedItems ? JSON.parse(storedItems) : null;
+  });
+  const [currentPage, setCurrentPage] = useState<number>(1);
 
-  useEffect(() => {
-    post(url, xAuth, "get_ids", { offset: 10, limit: 50 })
-      .then((data: unknown) => {
-        const itemsData = data as string[];
-        return post(url, xAuth, "get_items", { ids: itemsData });
-      })
-      .then((data: unknown) => {
-        const itemsData = data as ItemProps[];
-
-        // Фильтруем повторяющиеся id из массива
-        const uniqueItems = filterUniqueItems(itemsData);
-        setItems(uniqueItems);
-      })
-      .catch((error) => console.error("Error fetching data:", error));
-  }, []);
-
-  useEffect(() => {
-    post(url, xAuth, "get_fields").then((data: unknown) => {
-      const itemsData = data as string[];
-      console.log(itemsData);
+  const fetchItems = async (page: number) => {
+    const data = await post(url, xAuth, "get_ids", {
+      offset: (page - 1) * 50,
+      limit: 50,
     });
+    const itemsData = data as string[];
+    const itemsDetails = await post(url, xAuth, "get_items", {
+      ids: itemsData,
+    });
+    return itemsDetails as ItemProps[];
+  };
+
+  const loadPage = async (page: number) => {
+    try {
+      const itemsData = await fetchItems(page);
+      const uniqueItems = filterUniqueItems(itemsData);
+      setItems(uniqueItems);
+      setCurrentPage(page);
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
+  };
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const data = await post(url, xAuth, "get_ids", {
+          offset: 0,
+          limit: 50,
+        });
+        const itemsData = data as string[];
+        console.log(itemsData);
+
+        await loadPage(currentPage);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+    fetchData();
   }, []);
+
+  const loadNextPage = () => {
+    const nextPage = currentPage + 1;
+    loadPage(nextPage);
+  };
+
+  const loadPrevPage = () => {
+    const prevPage = currentPage - 1;
+    loadPage(prevPage);
+  };
+
+  useEffect(() => {
+    localStorage.setItem("items", JSON.stringify(items));
+  }, [items]);
 
   return (
     <>
       <Header />
-      <FilterBlock url={url} xAuth={xAuth} post={post} loading={loading} />
+      <FilterBlock
+        url={url}
+        xAuth={xAuth}
+        post={post}
+        loading={loading}
+        setItems={setItems}
+      />
+      <Pagination
+        currentPage={currentPage}
+        onPageChange={loadPage}
+        onLoadNextItems={loadNextPage}
+        onLoadPrevItems={loadPrevPage}
+      />
       <div className="wrapper">
         <ItemsList items={items} loading={loading} />
       </div>
