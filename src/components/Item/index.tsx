f4cc891329a8d875063e2b8f2110abca0ea3ee5a
Danilov_Nikolai <dan1k91@mail.ru>
2024-03-05 16:08:48 +0300

    decomposition

--- src/components/Item/index.tsx
+++ src/components/Item/index.tsx
@@ -1,40 +1,23 @@
 import styles from "./Item.module.scss";
 import { ItemProps } from "../../App";
-import ring from "../../assets/ring.png";
-import necklace from "../../assets/necklace.png";
-import earrings from "../../assets/earrings.png";
-import pendant from "../../assets/pendant.png";
-import bracelet from "../../assets/bracelet.webp";
-import gemstone from "../../assets/gemstone.png";
+import images from "./images";
 
 const Item: React.FC<ItemProps> = ({ brand, id, price, product }) => {
-  const images = [
-    { condition: product.includes("кольцо"), src: ring },
-    { condition: product.includes("серьги"), src: earrings },
-    {
-      condition: product.includes("ожерелье") || product.includes("колье"),
-      src: necklace,
-    },
-    { condition: product.includes("кулон"), src: pendant },
-    { condition: product.includes("браслет"), src: bracelet },
-  ];
-
-  const itemImage = images.find((image) => image.condition)?.src || gemstone;
+  const itemImage =
+    images.find((image) => image.condition(product))?.src || images[0].src;
 
   return (
     <div className={styles.root}>
       <div className={styles.item_container}>
         <img className={styles.item_image} src={itemImage} alt="item" />
         <h3 className={styles.item_title}>{product}</h3>
-        <div className={styles.item_id}>
-          <b>id:</b> {id}
+        <div className={styles.item_brand}>
+          {brand ? `Бренд: ${brand}` : "Без бренда"}
         </div>
-        {brand ? (
-          <div className={styles.item_brand}>{brand}</div>
-        ) : (
-          <div className={styles.item_brand}>Без бренда</div>
-        )}
         <div className={styles.item_price}>{price} ₽</div>
+      </div>
+      <div className={styles.item_id}>
+        <b>id:</b> {id}
       </div>
     </div>
   );
